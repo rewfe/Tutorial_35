9776a0fb7ac226a1f2878955c83f43e8debd1ec7
rewfe <qrewfe@yandex.ru>
2015-06-19 04:42:17 +0300

    basic blocking

--- Tutorial_35/ViewController.m
+++ Tutorial_35/ViewController.m
@@ -16,7 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    int (^howMany)(int, int) = ^(int a, int b){
+        return a + b;
+    };
+    
+    NSLog(@"%d", howmany(5, 10));
+    
+    
+    NSDate *(^today)(void);
+    
+    today = ^(void){
+        return [NSDate date];
+    };
+    
+    NSLog(@"%@", today());
+    
+    
+    float results = ^(float value1, float value2, float value3){
+        return value1 * value2 * value3;
+    } (1.2, 3.4, 5.6);
+    
+    NSLog(@"%f", results);
+    
+    
+    int factor = 5;
+    int (^newResult)(void) = ^(void){
+        return factor * 10;
+    };
+    
+    NSLog(@"%d", newResult());
+    
+        // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
